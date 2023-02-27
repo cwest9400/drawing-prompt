6a1876ba2047f26911bbbf66d00a914ce27d566e
cwest9400 <chrstphrwest@gmail.com>
2023-02-27 11:35:51 -0500

    openAI setup

--- api/generate.js
+++ api/generate.js
@@ -3,3 +3,28 @@ import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+export default async function (req, res) {
+    if (!configuration.apiKey) {
+        res.status(500).json({
+            error: {
+                message: "OpenAI API key not configured"
+            }
+        });
+        return;
+    }
+
+    const animal = req.body.animal || "";
+    if (animal.trim().length === 0){
+        res.status(400).json({
+            error: {
+                message: "Please enter a base animal for the prompt"
+            }
+        });
+        return;
+    }
+
+    try {
+        
+    }
+}
