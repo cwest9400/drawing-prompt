3ad1f8c6c2d3cfc3128693043ce4ca5a9f5f5f14
cwest9400 <chrstphrwest@gmail.com>
2023-03-14 10:50:45 -0400

    navbar styling

--- components/NavbarMain.js
+++ components/NavbarMain.js
@@ -1,11 +1,11 @@
-
+import styles from '../styles/NavbarMain.module.css';
 
 import Link from "next/link"
 
 export default function NavbarMain() {
   
     return (
-        <div>
+        <div className='styles.navbar'>
             
             <Link href="/promptsPage">Word Prompt</Link>
             <Link href="/imagesPage">Image Promt</Link>
