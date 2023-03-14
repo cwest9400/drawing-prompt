8afe9f07fb59c7407b906c2c727374af42d98aaf
cwest9400 <chrstphrwest@gmail.com>
2023-03-14 09:58:50 -0400

    added imagespage

---
+++ pages/imagesPage.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function imagesPage() {
+  
+    return (
+        <div>
+            <h1>images page</h1>
+            <Link href="/index">back to home</Link>
+        </div>
+    )
+}
