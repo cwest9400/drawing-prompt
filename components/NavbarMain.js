2b75482b463888954a23d624bf5c40b71232df95
cwest9400 <chrstphrwest@gmail.com>
2023-03-14 10:03:48 -0400

    added basic main navbar placeholder

---
+++ components/NavbarMain.js
@@ -0,0 +1,15 @@
+
+
+import Link from "next/link"
+
+export default function NavbarMain() {
+  
+    return (
+        <div>
+            
+            <Link href="/index">Word Prompt</Link>
+            <Link href="/index">Image Promt</Link>
+            <Link href="">Sign In</Link>
+        </div>
+    )
+}
