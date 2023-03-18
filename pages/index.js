e6165bef0d030ee2f28b8150934d8c3911c8b897
cwest9400 <chrstphrwest@gmail.com>
2023-03-17 21:02:39 -0400

    styling

--- pages/index.js
+++ pages/index.js
@@ -6,124 +6,87 @@ import Navbar from '../components/Navbar';
 // import styles from "../styles/prompt.module.css";
 
 export default function Home() {
-  
+
   return (
-    <div className={styles.main}>
+    <>
       <Head>
         <title>Daily Doodley Draw</title>
-
       </Head>
-      <nav> 
-        <Navbar />
-      </nav>
+    
       <main className={styles.main}>
-      <div className={styles.description}>
-        <h1>
-          Doodley Draw Daily
-        </h1>
-      <p>
-          Doodle for 10 minutes a day to keep the feel bads away.  
-        </p>
-        
-      </div>
+        <div className={styles.description}>
+          <h1>
+            Doodley Draw Daily
+          </h1>
+          <p>
+            Doodle for 10 minutes a day to keep the feel bads away.
+          </p>
+        </div>
+      </main>
+      <Navbar />
+      <footer>
 
-    </main>
-    </div>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
+      `}</style>
+    
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+            background-color: black;
+            color: whitesmoke;
+        }
+        * {
+          box-sizing: border-box;
+        }
+        a {
+          text-decoration: none;
+          color: whitesmoke;
+        }
+      `}</style>
+    </>
   )
-  // <div className={styles.container}>
-  //   <Head>
-  //     <title>Drawing Prompts</title>
-  //     <link rel="icon" href="/favicon.ico" />
-  //   </Head>
-
-  //   <main>
-  //     <h1 className={styles.title}>
-  //       Welcome to <a href="">Drawing Prompts!</a>
-  //     </h1>
-
-  //     <p className={styles.description}>
-  //       Get started by choosing a prompt!
-  //     </p>
-
-  //     <div className={styles.grid}>
-  //       <div className={styles.card}>
-  {/* <AnimalPrompt /> */ }
-
-
-  {/* <h3>Draw an Animal! &darr;</h3> */ }
-  {/* <p>Find in-depth information about Next.js features and API.</p> */ }
-  //         </div>
 
 
 
-
-
-
-
-  //       </div>
-  //     </main>
-
-  //     <footer>
-  //       <a
-  //         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Powered by{' '}
-  //         <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-  //       </a>
-  //     </footer>
-
-  //     <style jsx>{`
-  //       main {
-  //         padding: 5rem 0;
-  //         flex: 1;
-  //         display: flex;
-  //         flex-direction: column;
-  //         justify-content: center;
-  //         align-items: center;
-  //       }
-  //       footer {
-  //         width: 100%;
-  //         height: 100px;
-  //         border-top: 1px solid #eaeaea;
-  //         display: flex;
-  //         justify-content: center;
-  //         align-items: center;
-  //       }
-  //       footer img {
-  //         margin-left: 0.5rem;
-  //       }
-  //       footer a {
-  //         display: flex;
-  //         justify-content: center;
-  //         align-items: center;
-  //         text-decoration: none;
-  //         color: inherit;
-  //       }
-  //       code {
-  //         background: #fafafa;
-  //         border-radius: 5px;
-  //         padding: 0.75rem;
-  //         font-size: 1.1rem;
-  //         font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-  //           DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-  //       }
-  //     `}</style>
-
-  //     <style jsx global>{`
-  //       html,
-  //       body {
-  //         padding: 0;
-  //         margin: 0;
-  //         font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-  //           Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-  //           sans-serif;
-  //       }
-  //       * {
-  //         box-sizing: border-box;
-  //       }
-  //     `}</style>
-  //   </div>
-  // )
 }
