7200e5695a14517730b439a7b451fcee5275cb6e
cwest9400 <chrstphrwest@gmail.com>
2023-03-27 23:10:27 -0400

    refactor images page

--- pages/imagesPage.js
+++ pages/imagesPage.js
@@ -1,18 +1,30 @@
 import Link from "next/link"
 import RandomImage from "../components/RandomImage"
 import Navbar from "../components/Navbar"
+import styles from '../styles/Navbar.module.css';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function imagesPage() {
-  
+
     return (
         <div>
             <Navbar />
-            
+
             <Link href="/">back to home</Link>
-<div>
-    <div></div>
-    <RandomImage />
-</div>
+            <div>
+                <div>
+                    <Link href="" className={styles.card}>
+                        <h3 className={inter.className}>Categories</h3>
+                    </Link>
+                    <Link href="" className={styles.card}>
+                    <h3 className={inter.className}>Random</h3>
+                </Link>
+                </div>
+
+                {/* <RandomImage /> */}
+            </div>
 
         </div>
     )
